b2edf8a9bcc116ee8878e3cd16e9838ad19c2393
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-08-23 21:12:23 +0530

    Partial Template Including

--- challanges/urls.py
+++ challanges/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     # path("january", views.january),
     #  path("february", views.february)
-    path("",  views.index), 
+    path("",  views.index, name="index"), 
     path("<int:month>",views.monthly_challanges_by_number),
     path("<str:month>",views.monthy_challanges_by_string, name="month-challange")
 
