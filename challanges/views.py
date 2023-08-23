8dc6cf670204eca85d9a12545bee62a368587c1b
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-08-23 21:22:55 +0530

    404 Template

--- challanges/views.py
+++ challanges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
+
 # from django.template.loader import render_to_string
 
 
@@ -73,4 +74,5 @@ def monthy_challanges_by_string(request, month):
         })
 
     except:
-        return HttpResponseNotFound("<h1>This month is not supported</h1>")
+        # return HttpResponseNotFound("<h1>This month is not supported</h1>")
+        raise Http404()
